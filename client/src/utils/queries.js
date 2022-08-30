aa4c26b9f9b006c1ff72c044eab4558e156f3bcd
Gedion Adamu <gedionadamu@gmail.com>
2022-08-30 11:47:14 -0400

    making sure code is uptodate

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,18 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USERS = gql`
-  query users {
-    users {
-      _id
-      name
-      email
-    }
-  }
-`;
+// me query for login 
 export const GET_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      bookCount
-      savedBooks
-    }
-  }
+    query me {
+        me {
+            _id
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+                title 
+                image
+            }
+        }
+    } 
 `;
